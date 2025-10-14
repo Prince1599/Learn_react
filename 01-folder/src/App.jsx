714b3b01b894e6f9526254ad3096fdea42c2e55e
Prince Yadav <prince.data7@gmail.com>
2025-10-14 15:26:55 +0530

    learn react

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+  return (
+    <>
+    <div id="parent">
+      <h1 id="child1">App</h1>
+      <h2 id="child2">Hello World</h2>
+      
+    </div>
+    <div id="chacha"></div>
+    </>
+  )
+}
+
+export default App
